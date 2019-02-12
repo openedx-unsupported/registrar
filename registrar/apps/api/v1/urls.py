d813ed087e38c40612b48c609592f7fbc395c50e
Alex Dusenbery <adusenbery@edx.org>
2019-02-12 10:14:09 -0500

    Create a basic program read-only view to be proud of.

--- registrar/apps/api/v1/urls.py
+++ registrar/apps/api/v1/urls.py
@@ -1,10 +1,12 @@
 """ API v1 URLs. """
-# pylint: disable=unused-import
-from django.conf.urls import url
 from rest_framework import routers
 
-from .views import ProgramEnrollmentView
+from .views import ProgramReadOnlyViewSet
 
 
 app_name = 'v1'
-urlpatterns = []
+
+router = routers.DefaultRouter()
+router.register(r'programs', ProgramReadOnlyViewSet, basename='program')
+
+urlpatterns = router.urls
