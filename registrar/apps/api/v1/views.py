0674b9925382c6ad93918657d9c58a8dc9d1f03f
Alex Dusenbery <adusenbery@edx.org>
2019-02-12 10:14:09 -0500

    Create a basic program read-only view to be proud of.

--- registrar/apps/api/v1/views.py
+++ registrar/apps/api/v1/views.py
@@ -1,15 +1,29 @@
 """
 The public-facing REST API.
 """
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import viewsets
 
-from registrar.apps.enrollments.models import LearnerProgramEnrollment
-from registrar.apps.api.serializers import LearnerProgramEnrollmentSerializer
+from registrar.apps.enrollments.models import Program
+from registrar.apps.api.serializers import ProgramSerializer
 
 
-class ProgramEnrollmentView(APIView):
-    def get(self, request, discovery_uuid=None, learner_id=None):
-        if discovery_uuid:
-            enrollments = LearnerProgramEnrollment.objects.filter(program__discovery_uuid=discovery_uuid)
-            return Response(LearnerProgramEnrollmentSerializer(enrollments, many=True).data)
+class ProgramReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    A view for accessing program objects.
+
+    retrieve:
+    Return the program associated with the given ``discovery_uuid``,
+    or 404 if no such program exists.
+
+    list:
+    Return all programs associated with the organization making the request.
+
+    Usage:
+      * /api/v1/programs/{discovery_uuid}/  - Returns data for a single program, given the program's discovery UUID
+      * /api/v1/programs/ - Returns a list of all programs the requesting user has access to.
+    """
+    # This field specifies that individual programs should
+    # be filtered by their `discovery_uuid` field.
+    lookup_field = 'discovery_uuid'
+    queryset = Program.objects.all()
+    serializer_class = ProgramSerializer
