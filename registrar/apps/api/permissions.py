1b60d48653f99fc46e252d835615f1f1c410e04c
Kyle McCormick <kmccormick@edx.org>
2019-02-21 10:59:48 -0500

    wip! Finish implementation

--- registrar/apps/api/permissions.py
+++ registrar/apps/api/permissions.py
@@ -2,49 +2,13 @@
 Utilities related to API permissions.
 """
 
-from rest_framework import permissions
-
-class ProgramReadOnlyViewSetPermission(permissions.BasePermission):
+def get_user_organization_keys(user):
     """
-    Limit users to only be able to view program listings for
-    organizations with which they are affiliated.
-    """
-    message = 'You are not permitted to view this set of programs.'
-
-    def has_permission(self, request, view):
-        url = request.get_full_path()
-        is_list_endpoint = (
-            url.endswith('programs') or
-            url.endswith('programs/')
-        )
-        # If we are querying individual objects, let has_object_permission handle it
-        if not is_list_endpoint:
-            return True
-        org_key = request.GET.get('org', None)
-        return can_user_access_organization(request.user, org_key)
-
-    def has_object_permission(self, request, view, program):
-        for org in program.organizations.all():
-            if can_user_access_organization(request.user, org):
-                return True
-        else:
-            return False
-
-
-def can_user_access_organization(user, org_key):
-    """
-    Returns whether the given user can perform operations on a given
-    organization.
+    Get set of keys for organizations the user is affiliated with.
 
     Arguments:
         user (User)
-        org_key (str): if None, refers to 'all organizations'
 
-    Returns: bool
+    Returns: set[str]
     """
-    if user.is_staff:
-        return True
-    if org_key is None:
-        return False
-    org_key_lower = org_key.lower()
-    return org_key_lower == 'gt'  # TODO: Finish writing this function
+    return set(['gt', 'uta'])  # TODO: Write this function
--- registrar/apps/api/v1/views.py
+++ registrar/apps/api/v1/views.py
@@ -1,12 +1,14 @@
 """
 The public-facing REST API.
 """
+from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from registrar.apps.enrollments.models import Program, Organization
 from registrar.apps.api.serializers import ProgramSerializer
-from registrar.apps.api.permissions import ProgramReadOnlyViewSetPermission
+from registrar.apps.api.permissions import get_user_organization_keys
 
 
 class ProgramReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
@@ -22,16 +24,33 @@ class ProgramReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
 
     Usage:
       * /api/v1/programs/{program_key}/ - Returns data for a single program, given the program's key.
+      * /api/v1/programs - Returns a list of all programs the user has access to.
       * /api/v1/programs?org={org_key} - Returns a list of an organization's programs.
-      * /api/v1/programs - Returns a list of all programs. Staff users only.
     """
     lookup_field = 'key'
     serializer_class = ProgramSerializer
-    permission_classes = [ProgramReadOnlyViewSetPermission]
+    queryset = Program
 
-    def get_queryset(self):
-        org_key = self.request.GET.get('org', None)
+    def list(self, request):
+        org_key = request.GET.get('org', None)
         if org_key is None:
-            return Program.objects.all()
+            if not request.user.is_staff:
+                return HttpResponseForbidden()
+            programs = Program.objects.all()
         else:
-            return get_object_or_404(Organization, key=org_key).programs
+            if not request.user.is_staff:
+                user_keys = get_user_organization_keys(request.user)
+                if org_key.lower() not in user_keys:
+                    return HttpResponseForbidden()
+            programs = get_object_or_404(Organization, key=org_key).programs.all()
+        data = ProgramSerializer(programs, many=True).data
+        return Response(data)
+
+    def retrieve(self, request, key=None):
+        program = get_object_or_404(Program.objects.all(), key=key)
+        org_keys = set(org.key for org in program.organizations.all())
+        user_org_keys = get_user_organization_keys(request.user)
+        if len(org_keys.intersection(user_org_keys)) == 0:
+            return HttpResponseForbidden()
+        data = ProgramSerializer(program).data
+        return Response(data)
