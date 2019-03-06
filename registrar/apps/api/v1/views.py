0408314327ecce4e69dc1d5f91e2fe96ed528fec
Alex Dusenbery <adusenbery@edx.org>
2019-03-06 16:12:46 -0500

    clean up the permission-checking code a little

--- registrar/apps/api/v1/views.py
+++ registrar/apps/api/v1/views.py
@@ -4,7 +4,7 @@ The public-facing REST API.
 from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404
 from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
-from guardian.mixins import PermissionListMixin, PermissionRequiredMixin
+from guardian.mixins import PermissionRequiredMixin
 from rest_framework import viewsets
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.response import Response
@@ -13,7 +13,21 @@ from registrar.apps.api.serializers import ProgramSerializer
 from registrar.apps.enrollments.models import Program, Organization
 
 
-class RetrieveProgramViewSet(PermissionRequiredMixin, RetrieveModelMixin, viewsets.GenericViewSet):
+class BaseProgramViewSet(PermissionRequiredMixin, viewsets.GenericViewSet):
+    authentication_classes = (JwtAuthentication,)
+    serializer_class = ProgramSerializer
+    queryset = Program.objects.all()
+
+    # An authorized user must be able to read metadata about an organization's programs
+    permission_required = 'enrollments.organization_read_metadata'
+    # A staff user with the global permission for reading organization metadata can
+    # access the metadata for any organization.
+    accept_global_perms = True
+    # Return a 403 instead of trying to redirect the user
+    return_403 = True
+
+
+class RetrieveProgramViewSet(RetrieveModelMixin, BaseProgramViewSet):
     """
     A view for retrieving a single program object.
 
@@ -29,26 +43,24 @@ class RetrieveProgramViewSet(PermissionRequiredMixin, RetrieveModelMixin, viewse
         '404':
             description: Program key not found.
     """
-    authentication_classes = (JwtAuthentication,)
     lookup_field = 'key'
-    serializer_class = ProgramSerializer
-    queryset = Program
-
-    # An authorized user must be able to read metadata about an organization's programs
-    permission_required = 'enrollments.organization_read_metadata'
-    # A staff user with the global permission for reading organization metadata can
-    # access the metadata for any organization.
-    accept_global_perms = True
-    # Return a 403 instead of trying to redirect the user
-    return_403 = True
 
     @property
     def permission_object(self):
-        program = self.get_object()
+        """
+        Returns an organization object against which permissions should be checked.
+        If the requesting user does not have permission to access this program's
+        organization, or does not have global staff permissions, a 403
+        will eventually be returned.
+        """
+        try:
+            program = self.get_object().select_related('organization')
+        except:
+            return
         return program.managing_organization
 
 
-class ListProgramViewSet(ListModelMixin, PermissionRequiredMixin, viewsets.GenericViewSet):
+class ListProgramViewSet(ListModelMixin, BaseProgramViewSet):
     """
     A view for accessing program objects.
 
@@ -66,23 +78,18 @@ class ListProgramViewSet(ListModelMixin, PermissionRequiredMixin, viewsets.Gener
         '404':
             description: Program key not found.
     """
-    authentication_classes = (JwtAuthentication,)
-    lookup_field = 'key'
-    serializer_class = ProgramSerializer
-    queryset = Program.objects.all()
-
-    # An authorized user must be able to read metadata about an organization's programs
-    permission_required = 'enrollments.organization_read_metadata'
-    # A staff user with the global permission for reading organization metadata can
-    # access the metadata for any organization.
-    accept_global_perms = True
-    # Return a 403 instead of trying to redirect the user
-    return_403 = True
-
     @property
     def permission_object(self):
+        """
+        Returns an organization object against which permissions should be checked.
+        If the requesting user does not have permission to access the organization
+        specified by the ``org`` query parameter, or does not have global staff permissions,
+        a 403 will eventually be returned.
+        """
         org_key = self.request.GET.get('org')
         if not org_key:
+            # permission checking will pass only if the requesting
+            # user has global permissions to access any organization's data
             return
         try:
             return Organization.objects.get(key=org_key)
