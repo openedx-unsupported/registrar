eac5e389edb237ef7607e6f8f9588073cc867570
Kyle McCormick <kmccormick@edx.org>
2019-03-22 13:52:34 -0400

    Install django-user-tasks with simple test case

--- registrar/apps/enrollments/tasks.py
+++ registrar/apps/enrollments/tasks.py
@@ -6,6 +6,8 @@ from __future__ import absolute_import
 import logging
 
 from celery import shared_task
+from user_tasks.models import UserTaskArtifact
+from user_tasks.tasks import UserTask
 
 
 log = logging.getLogger(__name__)
@@ -18,3 +20,11 @@ def debug_task(self, *args, **kwargs):  # pylint: disable=unused-argument
     to the log as a DEBUG message.
     """
     log.debug('Request: {0!r}'.format(self.request))
+
+
+@shared_task(base=UserTask, bind=True)
+def debug_user_task(self, user_id, text):  # pylint: disable=unused-argument
+    """
+    A user task for debugging.  Creates user artifact containing given text.
+    """
+    UserTaskArtifact.objects.create(status=self.status, text=text)
--- registrar/apps/enrollments/tests/test_tasks.py
+++ registrar/apps/enrollments/tests/test_tasks.py
@@ -3,14 +3,35 @@ Unit tests for the enrollment.tasks module.
 """
 import mock
 
+from django.test import TestCase
+from user_tasks.models import UserTaskArtifact, UserTaskStatus
+
+from registrar.apps.core.tests.factories import UserFactory
 from registrar.apps.enrollments import tasks
 
 
-@mock.patch('registrar.apps.enrollments.tasks.log', autospec=True)
-def test_debug_task_happy_path(mock_logger):
-    tasks.debug_task.apply_async([1, 2], kwargs={'foo': 'bar'})
+class DebugTaskTests(TestCase):
+    """ Tests for validating that tasks are working properly. """
 
-    assert 1 == mock_logger.debug.call_count
-    debug_call_argument = mock_logger.debug.call_args_list[0][0][0]
-    assert "'args': [1, 2]" in debug_call_argument
-    assert "'kwargs': {'foo': 'bar'}" in debug_call_argument
+    @mock.patch('registrar.apps.enrollments.tasks.log', autospec=True)
+    def test_debug_task_happy_path(self, mock_logger):
+        task = tasks.debug_task.apply_async([1, 2], kwargs={'foo': 'bar'})
+        task.wait()
+
+        self.assertEqual(mock_logger.debug.call_count, 1)
+        debug_call_argument = mock_logger.debug.call_args_list[0][0][0]
+        self.assertIn("'args': [1, 2]", debug_call_argument)
+        self.assertIn("'kwargs': {'foo': 'bar'}", debug_call_argument)
+
+    def test_debug_user_task_happy_path(self):
+        TEST_TEXT = "lorem ipsum"
+
+        user = UserFactory()
+        task = tasks.debug_user_task.apply_async((user.id, TEST_TEXT))
+        task.wait()
+
+        status = UserTaskStatus.objects.get(task_id=task.id)
+        self.assertEqual(status.state, UserTaskStatus.SUCCEEDED)
+
+        artifact = UserTaskArtifact.objects.get(status__user=user)
+        self.assertEqual(artifact.text, TEST_TEXT)
