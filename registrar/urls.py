e6420f64c13339a0d8b9f0163bd2ad184cfceb96
Kyle McCormick <kmccormick@edx.org>
2019-04-19 17:12:28 -0400

    Add trailing slash to api-docs

--- registrar/urls.py
+++ registrar/urls.py
@@ -31,7 +31,7 @@ app_name = 'registrar'
 urlpatterns = oauth2_urlpatterns + [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(api_urls)),
-    url(r'^api-docs', api_renderer.render_yaml_spec, name='api-docs'),
+    url(r'^api-docs/', api_renderer.render_yaml_spec, name='api-docs'),
     # Use the same auth views for all logins, including those originating from the browseable API.
     url(r'^api-auth/', include(oauth2_urlpatterns)),
     url(r'^auto_auth/$', core_views.AutoAuth.as_view(), name='auto_auth'),
