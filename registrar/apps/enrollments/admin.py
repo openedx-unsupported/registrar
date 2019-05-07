d6a7a01cd020ff6a6d37a3a446c3b98c09d3a859
Kyle McCormick <kmccormick@edx.org>
2019-05-07 14:48:30 -0400

    Remove declaration and references to unused learner models

--- registrar/apps/enrollments/admin.py
+++ registrar/apps/enrollments/admin.py
@@ -3,6 +3,4 @@ from django.contrib import admin
 
 from registrar.apps.enrollments import models
 
-admin.site.register(models.Learner)
-admin.site.register(models.LearnerProgramEnrollment)
 admin.site.register(models.Program)
--- registrar/apps/enrollments/models.py
+++ registrar/apps/enrollments/models.py
@@ -3,7 +3,6 @@ Models relating to learner program and course enrollments.
 """
 from django.db import models
 from model_utils.models import TimeStampedModel
-from simple_history.models import HistoricalRecords
 
 from registrar.apps.core.models import Organization
 
@@ -25,63 +24,3 @@ class Program(TimeStampedModel):
 
     def __str__(self):
         return self.title
-
-
-class Learner(TimeStampedModel):
-    """
-    Model that represents an LMS user who may be enrolled in programs and courses.
-
-    .. pii:: The ``email`` field contains pii in the form of the email address of a learner.
-    .. pii_types:: email_address
-    .. pii_retirement:: local_api
-    """
-    PENDING = 'pending'
-    ACCOUNT_CREATED = 'account_created'
-    STATUS_CHOICES = (
-        (PENDING, 'Pending'),
-        (ACCOUNT_CREATED, 'Account Created'),
-    )
-
-    lms_id = models.IntegerField(db_index=True, null=True)
-
-    # for storing id field provided by org
-    external_id = models.CharField(max_length=255, db_index=True, null=True)
-
-    email = models.EmailField(db_index=True)
-    status = models.CharField(
-        max_length=64,
-        choices=STATUS_CHOICES,
-        default=PENDING,
-    )
-    history = HistoricalRecords()
-
-
-class LearnerProgramEnrollment(TimeStampedModel):
-    """
-    Model that represents the relationship between a Learner and a Program.
-
-    .. no_pii::
-    """
-    class Meta(object):
-        app_label = 'enrollments'
-        unique_together = [
-            ('learner', 'program')
-        ]
-
-    PENDING = 'pending'
-    ENROLLED = 'enrolled'
-    STATUS_CHOICES = (
-        (PENDING, 'Pending'),
-        (ENROLLED, 'Enrolled'),
-    )
-
-    # we shouldn't be able to delete any of these fields out from under
-    # ourselves if an enrollment record is using them.
-    learner = models.ForeignKey(Learner, on_delete=models.PROTECT)
-    program = models.ForeignKey(Program, on_delete=models.PROTECT)
-    status = models.CharField(
-        max_length=64,
-        choices=STATUS_CHOICES,
-        default=PENDING,
-    )
-    history = HistoricalRecords()
