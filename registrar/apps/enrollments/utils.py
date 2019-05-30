4ad4623dcd7ebc786b36ae48c9669ce9236d688a
jansenk <jkantor@edx.org>
2019-05-30 11:18:18 -0400

    add function to check if there is an in-progress job for a program

---
+++ registrar/apps/enrollments/utils.py
@@ -0,0 +1,37 @@
+""" Utilities for the enrollments app """
+from user_tasks.models import UserTaskStatus
+
+
+USER_TASK_STATUS_PROCESSING_STATES = [
+    UserTaskStatus.PENDING,
+    UserTaskStatus.IN_PROGRESS,
+    UserTaskStatus.RETRYING,
+]
+
+
+def build_enrollment_job_status_name(program_key, task_name):
+    """
+    Build the UserTaskStatus.name for the given task and program
+
+    Arguments:
+        program_key (str): program key for the program we're writing enrollments
+        task_name (str): the name of the task that is being executed
+    """
+    return "{}:{}".format(program_key, task_name)
+
+
+def is_enrollment_job_processing(program_key):
+    """
+    Returns whether or not a bulk enrollment job for a particular program
+    is currently processing (in progress, pending, or retrying)
+
+    Used to ensure only one bulk task can be run at a time for a program.
+
+    Arguments:
+        program_key (str): program key for the program we're checking
+    """
+    program_prefix = build_enrollment_job_status_name(program_key, '')
+    return UserTaskStatus.objects.filter(
+        name__startswith=program_prefix,
+        state__in=USER_TASK_STATUS_PROCESSING_STATES,
+    ).exists()
