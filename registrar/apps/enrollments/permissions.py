2a36d1545112dd6fd2ac16ebc029b0a785c829fa
Simon Chen <schen@edx.org>
2019-11-21 15:00:29 -0500

    fix

--- registrar/apps/enrollments/permissions.py
+++ registrar/apps/enrollments/permissions.py
@@ -12,53 +12,11 @@ PROGRAM_READ_ENROLLMENTS_KEY = 'program_read_enrollments'
 # Non-prefixed names of program enrollment write permissions
 PROGRAM_WRITE_ENROLLMENTS_KEY = 'program_write_enrollments'
 
-# A user with this permission can read program data report associated
-# with the program. This permission do not allow program enrollments
-# nor program course enrollment reads or writes
+# These permissions should not be assigned to any use on Registrar.
+# These are the permissions we created to prevent any users from accessing
+# the enrollments reports on Micromasters programs. Registrar right now
+# does not facilitate the enrollments of Micromasters programs, hence
+# there is no enrollment mechanism that is valid associated with Micromasters
+# programs. No user should have the permissions below
 PROGRAM_READ_ENROLLMENTS = APP_PREFIX + PROGRAM_READ_ENROLLMENTS_KEY
 PROGRAM_WRITE_ENROLLMENTS = APP_PREFIX + PROGRAM_WRITE_ENROLLMENTS_KEY
-
-
-
-
-class ProgramRole(object):
-    """
-    A collection of Program permissions that can be assigned
-    to a Group or ProgramGroup.
-    """
-
-    name = None
-    description = None
-    permissions = ()
-
-    @classmethod
-    def assign_to_group(cls, group, program):
-        """
-        For the given program, assigns all permissions under this role
-        to the given group.
-        """
-        for permission in cls.permissions:
-            assign_perm(permission, group, program)
-
-
-class ProgramReadEnrollmentsRole(ProgramRole):
-    """
-    The least-privileged program-scoped role, it may only
-    read enrollments about a program and its courses.
-    """
-    name = 'program_read_enrollments'
-    description = 'Read Enrollments Only'
-    permissions = (
-        PROGRAM_READ_ENROLLMENTS,
-    )
-
-class ProgramReadWriteEnrollmentsRole(ProgramRole):
-    """
-    The read and write privileged program-scoped role, it may read
-    and write enrollments about a program and its courses.
-    """
-    name = 'program_read_write_enrollments'
-    description = 'Read and Write Enrollments'
-    permissions = (
-        PROGRAM_READ_ENROLLMENTS, PROGRAM_WRITE_ENROLLMENTS,
-    )
