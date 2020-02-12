20097b27eb6b3b29ebd39110fcfcae34673b4424
Chris Pappas <christopappas@users.noreply.github.com>
2020-02-12 09:59:39 -0500

    getting consumner to run via management command; (#258)

--- registrar/consumer.py
+++ registrar/consumer.py
@@ -1,39 +1,43 @@
- """
- Defines the Kombu consumer for the registrar project.
- """
- from __future__ import absolute_import
+"""
+Defines the Kombu consumer for the registrar project.
+"""
+from __future__ import absolute_import
 
- from kombu.mixins import ConsumerMixin
- from kombu import Exchange, Queue
+from kombu.mixins import ConsumerMixin
+from kombu import Exchange, Queue
 
- task_exchange = Exchange('course_discovery', type='direct')
- queues = [
-     Queue('task_queue', task_exchange, routing_key='task_queue'),
- ]
+from registrar.apps.core.models import Organization
 
- class Worker(ConsumerMixin):
+task_exchange = Exchange('course_discovery', type='direct')
+queues = [
+    Queue('task_queue', task_exchange, routing_key='task_queue'),
+]
 
-     def __init__(self, connection):
-         self.connection = connection
+class Worker(ConsumerMixin):
 
-     def get_consumers(self, Consumer, channel):
-         return [
-             Consumer(queues, callbacks=[self.on_message], accept=['json']),
-         ]
+    def __init__(self, connection):
+        self.connection = connection
 
-     def on_message(self, body, message):
-         print('RECEIVED MESSAGE: {0!r}'.format(body))
-         message.ack()
+    def get_consumers(self, Consumer, channel):
+        return [
+            Consumer(queues, callbacks=[self.on_message], accept=['json']),
+        ]
+
+    def on_message(self, body, message):
+        print(Organization.objects.first())
+        print("If this prints, then we can access Django models!")
+        print('RECEIVED MESSAGE: {0!r}'.format(body))
+        message.ack()
 
 
- if __name__ == '__main__':
-     from kombu import Connection
-     from kombu.utils.debug import setup_logging
-     setup_logging(loglevel='DEBUG')
+def run_consumer_worker():
+    from kombu import Connection
+    from kombu.utils.debug import setup_logging
+    setup_logging(loglevel='DEBUG')
 
-     with Connection('redis://:password@redis:6379/0') as conn:
-         try:
-             Worker(conn).run()
-         except KeyboardInterrupt:
-             print('bye bye')
+    with Connection('redis://:password@redis:6379/0') as conn:
+        try:
+            Worker(conn).run()
+        except KeyboardInterrupt:
+            print('bye bye')
 
