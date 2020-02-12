fb271012db41657c576bab8f34eec3f48da66586
chavesj <jchaves@edx.org>
2020-02-12 11:49:16 -0500

    quality fixes

--- registrar/apps/core/consumers.py
+++ registrar/apps/core/consumers.py
@@ -3,14 +3,17 @@ Defines the Kombu consumer for the registrar project.
 """
 from __future__ import absolute_import
 
-from django.conf import settings
-from kombu.mixins import ConsumerMixin
 from kombu import Exchange, Queue
+from kombu.mixins import ConsumerMixin
 
 from registrar.apps.core.models import Program
 
 
 class ProgramConsumer(ConsumerMixin):
+    """
+    Class to define Kombu consumer behavior.
+    Consumer Queue binds to an exchange and handles messages from corresponding Producers.
+    """
     exchange = Exchange('course_discovery', type='direct')
     queues = [
         Queue('task_queue', exchange, routing_key='task_queue'),
@@ -21,7 +24,7 @@ class ProgramConsumer(ConsumerMixin):
 
     def get_consumers(self, Consumer, channel):
         return [
-            Consumer(queues, callbacks=[self.on_message], accept=['json']),
+            Consumer(self.queues, callbacks=[self.on_message], accept=['json']),
         ]
 
     def on_message(self, body, message):
@@ -29,4 +32,3 @@ class ProgramConsumer(ConsumerMixin):
         print('RECEIVED MESSAGE: {0!r}'.format(body))
 
         message.ack()
-
--- registrar/apps/core/management/commands/run_consumer.py
+++ registrar/apps/core/management/commands/run_consumer.py
@@ -2,27 +2,25 @@
 import logging
 
 from django.conf import settings
-from django.contrib.auth.models import Group
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from kombu import Connection
 from kombu.utils.debug import setup_logging
 
 from registrar.apps.core.consumers import ProgramConsumer
 
+
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    # pylint: disable=missing-docstring
-
-    help = 'Runs a worker to act on messages received from queue.'
-
+    """
+    Runs a worker to act on messages received from queue.
+    """
     def handle(self, *args, **options):
         setup_logging(loglevel='DEBUG')
 
         with Connection(settings.BROKER_URL) as conn:
-         try:
-             ProgramConsumer(conn).run()
-         except KeyboardInterrupt:
-             print('bye bye')
-
+            try:
+                ProgramConsumer(conn).run()
+            except KeyboardInterrupt:
+                print('bye bye')
