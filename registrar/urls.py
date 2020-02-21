0bd43c32863eacd84c94978e577e9d4c4cdd0e5a
Awais Qureshi <awais.qureshi@arbisoft.com>
2020-02-21 16:11:02 +0500

    Updated the make file to work with tox environments

--- registrar/urls.py
+++ registrar/urls.py
@@ -67,7 +67,8 @@ urlpatterns += [
 ]
 
 if settings.DEBUG and os.environ.get('ENABLE_DJANGO_TOOLBAR', False):  # pragma: no cover
-    import debug_toolbar
+    import debug_toolbar  # pylint: disable=import-error
+
     urlpatterns.append(url(r'^__debug__/', include(debug_toolbar.urls)))
 
 if settings.DEBUG:  # pragma: no cover
