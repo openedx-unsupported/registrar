f7f2fbf26172282590d9b31834e667e45cd5e54b
connorhaugh <49422820+connorhaugh@users.noreply.github.com>
2024-04-19 11:27:13 -0400

    feat: lint

--- registrar/celery.py
+++ registrar/celery.py
@@ -4,6 +4,7 @@ Defines the Celery application for the registrar project.
 
 from celery import Celery
 
+
 # TEMP: This code will be removed by ARCH-BOM on 4/22/24
 # ddtrace allows celery task logs to be traced by the dd agent.
 # TODO: remove this code.
